3e101a485559d218e53f1251a0fb10369c287bde
divino <viniaguiar9509@gmail.com>
2016-06-10 22:37:07 -0300

    Login, account folder, organization

--- Program/dotaStats.py
+++ Program/dotaStats.py
@@ -3,18 +3,25 @@
 
 ##################IMPORTS#####################
 import dota2api
+import os
 ##############################################
 
 ################GLOBAL VARIABLES################
 fileDirectory = "C:\\Users\\Vinícius\\Documents\\UFPE\\Coding\\Python\\Dota\\txtResults\\"
+accountDirectory = "C:\\Users\\Vinícius\\Documents\\UFPE\\Coding\\Python\\Dota\\Accounts\\"
 ################################################
 
 # First thing to code: Write down all the relevant info about one player in one match in a txt file
 
-def getMatchHistory(accountID):
+def findFile(name, path):
+	for root, dirs, files in os.walk(path):
+		if name in files:
+			return True
+
+def getMatchHistory(targetAccountID):
 	while (1):
 		try:
-			matchList = api.get_match_history(account_id=accountID)
+			matchList = api.get_match_history(account_id=targetAccountID)
 			print("We got the match history with success")
 			break
 		except:
@@ -25,7 +32,7 @@ def organizeMatches(matches, index):
 	match = matches[index]
 	return (match['match_id'])
 
-def getPlayerInfo(match, accountID):
+def getPlayerInfo(match, targetAccountID):
 	players = match['players']
 	player = players[2]
 	playerID = player['account_id']
@@ -35,7 +42,7 @@ def getPlayerInfo(match, accountID):
 	while(1):
 		player = players[x]
 		playerID = player['account_id']
-		if (playerID == accountID):
+		if (playerID == targetAccountID):
 			break
 		x = x + 1
 
@@ -55,14 +62,14 @@ def writeInFile(playerID, heroName, killNumber, assistNumber, deathNumber, lastH
 		toCreate.write(killNumber + "/" + deathNumber + "/" + assistNumber + "\n")
 		toCreate.write(lastHits + "/" + denies + "\n\n")
 
-def getMatchDetails(matchList, accountID):
+def getMatchDetails(matchList, targetAccountID):
 	matches = matchList['matches']
 	index = matchList['num_results']
-	for x in range(0, 10): # Change it up to index
+	for x in range(0, index): # Change it up to index
 		matchAux = organizeMatches(matches, x)
 		while(1):
 			try:
-				getPlayerInfo(api.get_match_details(matchAux), accountID)
+				getPlayerInfo(api.get_match_details(matchAux), targetAccountID)
 				print("We got match number " + str(x + 1) + " with success")
 				break
 			except:
@@ -74,13 +81,26 @@ def greetings():
 	option = int(input())
 	return option
 
+def login():
+	print("What's YOUR account ID")
+	myAccountID = int(input())
+	if (findFile(str(myAccountID) + ".txt", accountDirectory)):
+		return myAccountID
+	else:
+		print("It's your first time here, let me create your folder")
+		print("We will always save what you're doing in your personal folder")
+		with open(accountDirectory + str(myAccountID) + ".txt", "a") as createAccount:
+			createAccount.write("This file will store all the actions taken by the user " + str(myAccountID) + "\n\n\n")
+		return myAccountID
+
 def optionOne():
 	print("What's the account ID?")
-	accountID = int(input())
-	matchList = getMatchHistory(accountID)
-	getMatchDetails(matchList, accountID)
+	targetAccountID = int(input())
+	matchList = getMatchHistory(targetAccountID)
+	getMatchDetails(matchList, targetAccountID)
 
 def main():
+	myAccountID = login()
 	option = greetings()
 	if (option == 0):
 		return
@@ -88,6 +108,7 @@ def main():
 		optionOne()
 
 ##########################RUNNER##########################
+print("What is your API Key?")
 apiKey = input()
 api = dota2api.Initialise(apiKey)
 main()
